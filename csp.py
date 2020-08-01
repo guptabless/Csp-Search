a792a592efe233059c799bf92b90232d17e7c6ff
guptabless <61534368+guptabless@users.noreply.github.com>
2020-08-01 01:13:05 -0400

    Create csp.py

---
+++ csp.py
@@ -0,0 +1,52 @@
+import requests
+import sys, argparse
+import bcolors
+
+
+def banner():
+    print("""
+
+
+                ░█████╗░░██████╗██████╗░░░░░░░░██████╗███████╗░█████╗░██████╗░░█████╗░██╗░░██╗
+                ██╔══██╗██╔════╝██╔══██╗░░░░░░██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██║░░██║
+                ██║░░╚═╝╚█████╗░██████╔╝█████╗╚█████╗░█████╗░░███████║██████╔╝██║░░╚═╝███████║
+                ██║░░██╗░╚═══██╗██╔═══╝░╚════╝░╚═══██╗██╔══╝░░██╔══██║██╔══██╗██║░░██╗██╔══██║
+                ╚█████╔╝██████╔╝██║░░░░░░░░░░░██████╔╝███████╗██║░░██║██║░░██║╚█████╔╝██║░░██║
+                ░╚════╝░╚═════╝░╚═╝░░░░░░░░░░░╚═════╝░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝                                                                         
+                                                                                         Code by NG          
+              """)
+
+
+if len(sys.argv) > 1:
+    banner()
+    if (sys.argv[1] == '-u'):
+        try:
+            input_url = sys.argv[2]
+            input_header = requests.get(input_url)
+            h_text = input_header.headers['content-security-policy']
+
+            parser = argparse.ArgumentParser()
+            parser.add_argument("-u", required=True)
+            args = parser.parse_args()
+            print(bcolors.BITALIC + "Testing for Content Security Policy")
+
+            try:
+                  print(bcolors.OKMSG + 'Content Security Policy: ', h_text)
+            except:
+                  print("CSP not possible for the given URL")
+
+        except:
+            print('Please enter python CSP.py -u <URL>')
+            print("Give Domain with http:// or https://")
+
+    elif ((sys.argv[1] == '-h') | (sys.argv[1] == '--help')):
+        print(bcolors.BOLD + 'usage: CSP.py [-h] -d DOMAIN' '\n' 'OPTIONS:' '\n' '-h,--help    '
+                             'show this help message and exit' '\n''-u Url,   --url Url')
+else:
+    banner()
+    print(bcolors.ERR + 'Please select atleast 1 option from -u or -h, with a valid url')
+
+
+
+
+
